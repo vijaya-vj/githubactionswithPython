88e6669a86fef666dee8c6dc9a0f051cef5e3e25
vijaya-vj <vijayafive@gmail.com>
2021-10-23 18:38:17 +0000

    checkin the change for automated script

--- destination/AutomateQueries.sql
+++ destination/AutomateQueries.sql
@@ -1,1 +1,5 @@
-﻿--Generated scripts from flat file
+-- Training
+Insert into Training (Id,SessionName,Trainer,Date,Location) values (1, 'SQL', 'William', '2021-11-28T00:00:00.000000000', 'University');Insert into Training (Id,SessionName,Trainer,Date,Location) values (2, 'Azure', 'Kyle', '2021-11-30T00:00:00.000000000', 'University');Insert into Training (Id,SessionName,Trainer,Date,Location) values (3, 'Machine Learning', 'Austin', '2022-03-03T00:00:00.000000000', 'University');
+
+
+
