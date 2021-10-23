ca959ce99c15a1ce1cf09d9a58c8e15da621ab42
vijaya-vj <vijayafive@gmail.com>
2021-10-23 20:10:24 +0000

    modified values

--- destination/AutomateQueries.sql
+++ destination/AutomateQueries.sql
@@ -1,5 +1,5 @@
 -- Training
-Insert into Training (Id,SessionName,Trainer,Date,Location) values (1, 'SQL', 'William', '2021-11-28T00:00:00.000000000', 'University');Insert into Training (Id,SessionName,Trainer,Date,Location) values (2, 'Azure', 'Kyle', '2021-11-30T00:00:00.000000000', 'University');Insert into Training (Id,SessionName,Trainer,Date,Location) values (3, 'Machine Learning', 'Austin', '2022-03-03T00:00:00.000000000', 'University');
+Insert into Training (Id,SessionName,Trainer,Date,Location) values (1, 'SQL', 'William', '2021-11-28T00:00:00.000000000', 'University');Insert into Training (Id,SessionName,Trainer,Date,Location) values (2, 'Azure', 'Kyle', '2021-11-30T00:00:00.000000000', 'University');Insert into Training (Id,SessionName,Trainer,Date,Location) values (3, 'Machine Learning', 'Austin', '2022-03-03T00:00:00.000000000', 'University');Insert into Training (Id,SessionName,Trainer,Date,Location) values (4, 'Artifical Intelligence', 'Conrad', '2022-12-08T00:00:00.000000000', 'University');
 
 
 
